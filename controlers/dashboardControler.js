8ec7fda52eb08d0e2f43bb67b130ab5bd691427a
Ewerton Gronotte <ewertongronotte@gmail.com>
2019-12-23 17:25:51 +0400

    dcdf

--- controlers/dashboardControler.js
+++ controlers/dashboardControler.js
@@ -37,7 +37,7 @@ exports.login=(req, res, next)=>{
                         userName:user.userName,
                         role:user.role
                     }
-                    jwt.sign({ data:data},JWTKey,{expiresIn: '120ms'},(err,token)=>{
+                    jwt.sign({ data:data},JWTKey,{expiresIn: '60000ms'},(err,token)=>{
                         res.json(token)
                     })                    
                 }
