189a90f5b6d3aba8b983f4481e07bf0194c111ed
Ewerton Gronotte <ewertongronotte@gmail.com>
2019-12-27 15:30:23 +0400

    dcdf

--- routes/kitchen-kiosk.js
+++ routes/kitchen-kiosk.js
@@ -1,3 +1,4 @@
 const router = express.Router()
 const kitchenKioskControler = require('../controlers/kitchenKioskControler');
 router.post('/login', kitchenKioskControler.login)
+module.exports = router;
