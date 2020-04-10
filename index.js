2cad9e4c6ca16b7454d118b633eb41a7c3271471
Ewerton Gronotte <ewertongronotte@gmail.com>
2020-04-10 18:13:14 +0400

    fdfd

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './kiosk-dashboard/index.html'));
 });
 const PORT = process.env.PORT||80 ;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`); 
 });
 
